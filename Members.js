1495fb1118d3561082c054052e4d32c1d33ad410
VaderSteve76 <alverson.s@yahoo.com>
2019-09-25 21:12:24 -0400

    Add member

---
+++ Members.js
@@ -0,0 +1,28 @@
+const members = [
+  {
+    id: 1,
+    name: 'John Doe',
+    email: 'jon@gmail',
+    status: 'active'
+  },
+  {
+    id: 2,
+    name: 'Jane Doe',
+    email: 'jane@gmail.com',
+    status: 'inactive'
+  },
+  {
+    id: 3,
+    name: 'James Doe',
+    email: 'james@gmail.com',
+    status: 'active'
+  },
+  {
+    id: 4,
+    name: 'Jessie Doe',
+    email: 'jessie@gmail.com',
+    status: 'inactive'
+  }
+];
+
+module.exports = members;
--- index.js
+++ index.js
@@ -1,39 +1,20 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+//const logger = require('./middleware/Logger');
 
-const members = [
-  {
-    id: 1,
-    name: 'John Doe',
-    email: 'jon@gmail',
-    status: 'active'
-  },
-  {
-    id: 2,
-    name: 'Jane Doe',
-    email: 'jane@gmail.com',
-    status: 'inactive'
-  },
-  {
-    id: 3,
-    name: 'James Doe',
-    email: 'james@gmail.com',
-    status: 'active'
-  },
-  {
-    id: 4,
-    name: 'Jessie Doe',
-    email: 'jessie@gmail.com',
-    status: 'inactive'
-  }
-];
+// Init middleware
+//app.use(logger);
 
-app.get('/api/members', (req, res) => {
+// body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-});
-
+// set static folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+// members api routes
+app.use('/api/members', require('./Routes/API/Members'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
